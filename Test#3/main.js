908240352827848573a90fd9f83798fbbd86d620
Tram Pham <phamtramm99@gmail.com>
2022-10-01 16:44:46 +0700

    push code

--- Test#3/main.js
+++ Test#3/main.js
@@ -1,11 +1,12 @@
-( function ( $ ) {
+// w-template tab
+(function ($) {
     $(".w-template .filter li").each(function (index, elem) {
-    $(elem).click(function (e) {
-        console.log("hihi")
-        $("w-template .filter li.active ").removeClass("active");
-        $("w-template.list.active ").removeClass("active");
-        $(elem).addClass("active");
-        $("w-template .list")[index].classList.add("active");
-});
-});
-}( jQuery ) );
+        $(elem).click(function (e) {
+            console.log("hihi")
+            $(".w-template .filter li.tab ").removeClass("tab");
+            $(".w-template .list.tab ").removeClass("tab");
+            $(elem).addClass("tab");
+            $(".w-template .list")[index].classList.add("tab");
+        });
+    });
+}(jQuery));
